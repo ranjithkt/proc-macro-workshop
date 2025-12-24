585d2b42672818d3c8d4d292914034555f8cf92a
Ranjith Kumar <ranjithkt@hotmail.com>
2025-12-24 12:28:17 -0600

    6 tests passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Ident};
+use syn::{parse_macro_input, parse_quote, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, Ident, Visibility};
 
 // Before moving on, have the macro also generate:
 //
@@ -43,13 +43,34 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let name = &input.ident;
     let bname = format!("{}Builder", name);
     let bident = Ident::new(&bname, name.span());
+    let fields = if let Data::Struct(DataStruct {
+        fields: Fields::Named(FieldsNamed { ref named, .. }),
+        ..
+    }) = input.data
+    {
+        named
+    } else {
+        unimplemented!()
+    };
+
+    let optionized = fields.iter().map(|field| {
+        let ty = field.ty.clone();
+        let optional_ty = parse_quote! {
+            core::option::Option<#ty>
+        };
+        Field {
+            attrs: Vec::new(),
+            vis: Visibility::Inherited,
+            mutability: field.mutability.clone(),
+            ident: field.ident.clone(),
+            colon_token: field.colon_token,
+            ty: optional_ty,
+        }
+    });
 
     let expanded = quote! {
         pub struct #bident {
-            executable: Option<String>,
-            args: Option<std::vec::Vec<String>>,
-            env: Option<std::vec::Vec<String>>,
-            current_dir: Option<String>,
+            #(#optionized,)*
         }
         impl #bident {
             pub fn executable(&mut self, executable: String) -> &mut Self {
@@ -65,7 +86,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 self
             }
             pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
+                self.current_dir = Some(current_dir.into());
                 self
             }
             pub fn build(&mut self) -> Result<Command, std::boxed::Box<dyn std::error::Error>> {
@@ -73,7 +94,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     executable: self.executable.take().ok_or("executable not set")?,
                     args: self.args.take().ok_or("args not set")?,
                     env: self.env.take().ok_or("env not set")?,
-                    current_dir: self.current_dir.take().ok_or("current_dir not set")?,
+                    current_dir: self.current_dir.take().unwrap_or_default(),
                 })
             }
         }
