52a371a425da29a0cf65ccd9015eb7b920337ac9
Ranjith Kumar <ranjithkt@hotmail.com>
2025-12-24 09:50:49 -0600

    so far

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,94 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident};
+use syn::__private::Span;
+
+// Before moving on, have the macro also generate:
+//
+//     pub struct CommandBuilder {
+//         executable: Option<String>,
+//         args: Option<Vec<String>>,
+//         env: Option<Vec<String>>,
+//         current_dir: Option<String>,
+//     }
+//
+// and in the `builder` function:
+//
+//     impl Command {
+//         pub fn builder() -> CommandBuilder {
+//             CommandBuilder {
+//                 executable: None,
+//                 args: None,
+//                 env: None,
+//                 current_dir: None,
+//             }
+//         }
+//     }
+//
+//
+
+//     impl CommandBuilder {
+//         fn executable(&mut self, executable: String) -> &mut Self {
+//             self.executable = Some(executable);
+//             self
+//         }
+//
+//         ...
+//     }
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    eprintln!("{:#?}", input);
 
-    unimplemented!()
+    let name = &input.ident;
+    let bname = format!("{}Builder", name);
+    let bident = Ident::new(&bname, name.span());
+
+    let expanded = quote! {
+        pub struct #bident {
+            executable: Option<String>,
+            args: Option<alloc::vec::Vec<String>>,
+            env: Option<alloc::vec::Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #bident {
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+            pub fn build(&mut self) -> Result<Command, alloc::boxed::Box<dyn std::error::Error>> {
+                core::result::Result::Ok(#name {
+                    executable: self.executable.take().ok_or("executable not set")?,
+                    args: self.args.take().ok_or("args not set")?,
+                    env: self.env.take().ok_or("env not set")?,
+                    current_dir: self.current_dir.take().ok_or("current_dir not set")?,
+                })
+            }
+        }
+        impl #name {
+            pub fn builder() -> #bident {
+                #bident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    expanded.into()
 }
--- main.rs
+++ main.rs
@@ -1,9 +1,34 @@
-// Write code here.
+// Generate a `build` method to go from builder to original struct.
 //
-// To see what the code looks like after macro expansion:
-//     $ cargo expand
+// This method should require that every one of the fields has been explicitly
+// set; it should return an error if a field is missing. The precise error type
+// is not important. Consider using Box<dyn Error>, which you can construct
+// using the impl From<String> for Box<dyn Error>.
 //
-// To run the code:
-//     $ cargo run
+//     impl CommandBuilder {
+//         pub fn build(&mut self) -> Result<Command, Box<dyn Error>> {
+//             ...
+//         }
+//     }
 
-fn main() {}
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
+fn main() {
+    let mut builder = Command::builder();
+    builder.executable("cargo".to_owned());
+    builder.args(vec!["build".to_owned(), "--release".to_owned()]);
+    builder.env(vec![]);
+    builder.current_dir("..".to_owned());
+
+    let command = builder.build().unwrap();
+    assert_eq!(command.executable, "cargo");
+    println!("Are equal");
+}
