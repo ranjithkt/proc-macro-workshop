cda0042d9f7aa07d8c7c38c3bf031991aba3c67f
Ranjith Kumar <ranjithkt@hotmail.com>
2025-12-24 14:48:34 -0600

    accept String, but store in Option

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,10 @@
 use proc_macro::TokenStream;
+use quote::__private::ext::RepToTokensExt;
 use quote::quote;
-use syn::{parse_macro_input, parse_quote, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, Ident, Visibility};
+use syn::{
+    parse_macro_input, parse_quote, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed,
+    GenericArgument, Ident, Path, PathArguments, PathSegment, Type, TypePath, Visibility,
+};
 
 // Before moving on, have the macro also generate:
 //
@@ -53,11 +57,51 @@ pub fn derive(input: TokenStream) -> TokenStream {
         unimplemented!()
     };
 
+    fn is_ty_option(ty: &Type) -> bool {
+        if let Type::Path(TypePath {
+            path: Path { segments, .. },
+            ..
+        }) = ty
+        {
+            return !segments.is_empty() && segments.iter().last().unwrap().ident == "Option";
+        }
+
+        false
+    }
+
+    fn get_option_generic_type(ty: &Type) -> Option<&Type> {
+        if let Type::Path(TypePath {
+            path: Path { segments, .. },
+            ..
+        }) = ty
+        {
+            if let Some(last_ident) = segments.iter().last() {
+                if last_ident.ident == "Option" {
+                    if let PathArguments::AngleBracketed(ref arg) = last_ident.arguments {
+                        if let Some(GenericArgument::Type(s)) = arg.args.first() {
+                            return Some(s);
+                        }
+                    }
+                }
+            }
+        }
+
+        None
+    }
+
     let optionized = fields.iter().map(|field| {
         let ty = field.ty.clone();
-        let optional_ty = parse_quote! {
-            core::option::Option<#ty>
+
+        let optional_ty = if is_ty_option(&ty) {
+            parse_quote! {
+                #ty
+            }
+        } else {
+            parse_quote! {
+                core::option::Option<#ty>
+            }
         };
+
         Field {
             attrs: Vec::new(),
             vis: Visibility::Inherited,
@@ -70,26 +114,61 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let functions = fields.iter().map(|field| {
         let field_name = field.ident.clone();
-        quote! {
-           pub fn #field_name(&mut self, #field) -> &mut Self {
-               self.#field_name = Some(#field_name);
-               self
-           }
-       }
+        if is_ty_option(&field.ty) {
+            let inner_type = get_option_generic_type(&field.ty);
+
+            if let Some(inner_type) = inner_type {
+                let inner_field = Field {
+                    attrs: Vec::new(),
+                    vis: Visibility::Inherited,
+                    mutability: field.mutability.clone(),
+                    ident: field.ident.clone(),
+                    colon_token: field.colon_token,
+                    ty: inner_type.clone(),
+                };
+
+                quote! {
+                    pub fn #field_name(&mut self, #inner_field) -> &mut Self {
+                        self.#field_name = Some(#field_name);
+                        self
+                    }
+                }
+            } else {
+                quote! {
+                    pub fn #field_name(&mut self, #field) -> &mut Self {
+                        self.#field_name = Some(#field_name);
+                        self
+                    }
+                }
+            }
+        } else {
+            quote! {
+                pub fn #field_name(&mut self, #field) -> &mut Self {
+                    self.#field_name = Some(#field_name);
+                    self
+                }
+            }
+        }
     });
 
     let build_function = fields.iter().map(|field| {
         let field_name = field.ident.clone();
-        quote! {
-           #field_name: self.#field_name.take().unwrap_or_default()
-       }
+        if is_ty_option(&field.ty) {
+            quote! {
+                #field_name: self.#field_name.clone()?
+            }
+        } else {
+            quote! {
+                #field_name: self.#field_name.clone().ok_or(concat!(#field_name, " is not set"))?
+            }
+        }
     });
 
     let init_fields = fields.iter().map(|field| {
         let field_name = field.ident.clone();
         quote! {
-           #field_name: None
-       }
+            #field_name: None
+        }
     });
 
     let expanded = quote! {
